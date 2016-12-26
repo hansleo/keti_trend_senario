4af59899111eceabd1c58730b9448a34a1507049
hansleo <hansleo3589@gmail.com>
2016-12-26 17:29:12 +0900

    setup

---
+++ setup/setup_apt.sh
@@ -0,0 +1,21 @@
+﻿
+#Author : HanDong Lee (http://github.com/hansleo)
+
+sudo apt-get -y install htop
+sudo apt-get -y install libaio1
+sudo apt-get -y install build-essential
+sudo apt-get -y install unzip
+sudo apt-get -y install libaio-dev
+sudo apt-get -y install python-matplotlib
+sudo apt-get -y install python-mysqldb
+sudo apt-get -y install python-smbus
+sudo apt-get -y install python-requests
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-twisted
+sudo pip install requests --upgrade
+sudo pip install twisted --upgrade
+sudo pip install numpy
+sudo pip install networkx
+sudo pip install httplib
+sudo pip install urllib3
+sudo pip install utils
